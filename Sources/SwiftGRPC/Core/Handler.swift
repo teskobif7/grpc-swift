teskobif7/grpc-swift

commit 2fdc4dfce3b63ccf40f43e58a145a91555230c15

Ensure that errors in client calls are always provided to the user.
The expected behavior is now for the client call's completion handler to be called when the whole call is completed. You can check that completion handler's `statusCode` for the call's result. The intermediate send/receive call completion handlers will also get called with an error, but that error's status code is most likely to be `.unknown` (because send/receive calls don't receive the final status from the server).
In addition, `CallResult` now also includes the operation group's success flag, but that flag is much less important now.

This includes:
- Make most completion handlers non-throwing (their errors were for the most part discarded, anyway)
- Extract Receive-Streaming into a common protocol
- Changing many completion handler interfaces to be called with a `CallResult` instead of `Data?` and `ResultOrRPCError<ReceivedType?>` instead of `(ReceivedType, Error)`
- Not throwing on end of stream, but passing `nil` to the completion handler instead
- Making `ServerSessionUnary` always return an error when one of its calls fail
- Adding test to ensure the proper error behavior.

Sources/SwiftGRPC/Core/Handler.swift
@@ -30,7 +30,7 @@ public class Handler {
   public let requestMetadata: Metadata
 
   /// A Call object that can be used to respond to the request
-  private(set) lazy var call: Call = {
+  public lazy var call: Call = {
     Call(underlyingCall: cgrpc_handler_get_call(self.underlyingHandler),
          owned: false,
          completionQueue: self.completionQueue)
@@ -87,16 +87,16 @@ public class Handler {
   /// Receive the message sent with a call
   ///
   public func receiveMessage(initialMetadata: Metadata,
-                             completion: @escaping (Data?) throws -> Void) throws {
+                             completion: @escaping (Data?) -> Void) throws {
     let operations = OperationGroup(call: call,
                                     operations: [
                                       .sendInitialMetadata(initialMetadata),
                                       .receiveMessage
     ]) { operationGroup in
       if operationGroup.success {
-        try completion(operationGroup.receivedMessage()?.data())
+        completion(operationGroup.receivedMessage()?.data())
       } else {
-        try completion(nil)
+        completion(nil)
       }
     }
     try call.perform(operations)
@@ -152,11 +152,11 @@ public class Handler {
   /// - Parameter initialMetadata: initial metadata to send
   /// - Parameter completion: a completion handler to call after the metadata has been sent
   public func sendMetadata(initialMetadata: Metadata,
-                           completion: ((Bool) throws -> Void)? = nil) throws {
+                           completion: ((Bool) -> Void)? = nil) throws {
     let operations = OperationGroup(call: call,
                                     operations: [.sendInitialMetadata(initialMetadata)],
                                     completion: completion != nil
-                                      ? { operationGroup in try completion?(operationGroup.success) }
+                                      ? { operationGroup in completion?(operationGroup.success) }
                                       : nil)
     try call.perform(operations)
   }
@@ -165,7 +165,7 @@ public class Handler {
   ///
   /// - Parameter completion: a completion handler to call after the message has been received
   /// - Returns: a tuple containing status codes and a message (if available)
-  public func receiveMessage(completion: @escaping (Data?) throws -> Void) throws {
+  public func receiveMessage(completion: @escaping (CallResult) -> Void) throws {
     try call.receiveMessage(completion: completion)
   }
   
@@ -181,10 +181,10 @@ public class Handler {
   /// Recognize when the client has closed a request
   ///
   /// - Parameter completion: a completion handler to call after request has been closed
-  public func receiveClose(completion: @escaping (Bool) throws -> Void) throws {
+  public func receiveClose(completion: @escaping (Bool) -> Void) throws {
     let operations = OperationGroup(call: call,
                                     operations: [.receiveCloseOnServer]) { operationGroup in
-                                      try completion(operationGroup.success)
+                                      completion(operationGroup.success)
     }
     try call.perform(operations)
   }
@@ -197,7 +197,7 @@ public class Handler {
   /// - Parameter completion: a completion handler to call after the status has been sent
   public func sendStatus(statusCode: StatusCode,
                          statusMessage: String,
-                         trailingMetadata: Metadata,
+                         trailingMetadata: Metadata = Metadata(),
                          completion: ((Bool) -> Void)? = nil) throws {
     let operations = OperationGroup(call: call,
                                     operations: [
@@ -209,6 +209,14 @@ public class Handler {
       self.shutdown()
     }
     try call.perform(operations)
+  }
+  
+  public func sendError(_ error: ServerErrorStatus,
+                        completion: ((Bool) -> Void)? = nil) throws {
+    try sendStatus(statusCode: error.statusCode,
+                   statusMessage: error.statusMessage,
+                   trailingMetadata: error.trailingMetadata,
+                   completion: completion)
   }
 }
 

Sources/SwiftGRPC/Core/OperationGroup.swift
@@ -32,7 +32,7 @@ class OperationGroup {
   private let call: Call
 
   /// An array of operation objects that are passed into the initializer.
-  private let operations: [Operation]
+  let operations: [Operation]
 
   /// An array of observers used to watch the operation
   private var underlyingObservers: [UnsafeMutableRawPointer] = []
@@ -41,7 +41,7 @@ class OperationGroup {
   let underlyingOperations: UnsafeMutableRawPointer?
 
   /// Completion handler that is called when the group completes
-  let completion: ((OperationGroup) throws -> Void)?
+  let completion: ((OperationGroup) -> Void)?
 
   /// Indicates that the OperationGroup completed successfully
   var success = false
@@ -81,7 +81,7 @@ class OperationGroup {
   /// - Parameter operations: an array of operations
   init(call: Call,
        operations: [Operation],
-       completion: ((OperationGroup) throws -> Void)? = nil) {
+       completion: ((OperationGroup) -> Void)? = nil) {
     self.call = call
     self.operations = operations
     self.completion = completion

Sources/SwiftGRPC/Runtime/ServiceServer.swift
@@ -69,11 +69,9 @@ open class ServiceServer {
       do {
         if try !strongSelf.handleMethod(unwrappedMethod, handler: handler, queue: queue) {
           // handle unknown requests
-          try handler.receiveMessage(initialMetadata: Metadata()) { _ in
-            try handler.sendResponse(statusCode: .unimplemented,
-                                     statusMessage: "unknown method " + unwrappedMethod,
-                                     trailingMetadata: Metadata())
-          }
+          try handler.sendResponse(statusCode: .unimplemented,
+                                   statusMessage: "unknown method " + unwrappedMethod,
+                                   trailingMetadata: Metadata())
         }
       } catch (let error) {
         print("Server error: \(error)")

.gitignore
@@ -2,16 +2,11 @@
 project.xcworkspace
 xcuserdata
 .build
-protoc-gen-swift
-protoc-gen-swiftgrpc
+/protoc-gen-swift
+/protoc-gen-swiftgrpc
 third_party/**
-Plugin/Packages/**
-Plugin/Sources/protoc-gen-swiftgrpc/templates.swift
-Plugin/protoc-*
-Plugin/swiftgrpc.log
-Plugin/echo.*.swift
-Echo
-test.out
-echo.pid
-SwiftGRPC.xcodeproj
+/Echo
+/test.out
+/echo.pid
+/SwiftGRPC.xcodeproj
 Package.resolved

Sources/Examples/Echo/EchoProvider.swift
@@ -45,10 +45,9 @@ class EchoProvider: Echo_EchoProvider {
     var parts: [String] = []
     while true {
       do {
-        let request = try session.receive()
+        guard let request = try session.receive()
+          else { break }  // End of stream
         parts.append(request.text)
-      } catch ServerError.endOfStream {
-        break
       } catch (let error) {
         print("\(error)")
       }
@@ -63,7 +62,8 @@ class EchoProvider: Echo_EchoProvider {
     var count = 0
     while true {
       do {
-        let request = try session.receive()
+        guard let request = try session.receive()
+          else { break }  // End of stream
         var response = Echo_EchoResponse()
         response.text = "Swift echo update (\(count)): \(request.text)"
         count += 1
@@ -72,8 +72,6 @@ class EchoProvider: Echo_EchoProvider {
             print("update error: \(error)")
           }
         }
-      } catch ServerError.endOfStream {
-        break
       } catch (let error) {
         print("\(error)")
         break

Sources/Examples/Echo/Generated/echo.grpc.swift
@@ -33,9 +33,9 @@ fileprivate final class Echo_EchoGetCallBase: ClientCallUnaryBase<Echo_EchoReque
 
 internal protocol Echo_EchoExpandCall: ClientCallServerStreaming {
   /// Call this to wait for a result. Blocking.
-  func receive() throws -> Echo_EchoResponse
+  func receive() throws -> Echo_EchoResponse?
   /// Call this to wait for a result. Nonblocking.
-  func receive(completion: @escaping (Echo_EchoResponse?, ClientError?) -> Void) throws
+  func receive(completion: @escaping (ResultOrRPCError<Echo_EchoResponse?>) -> Void) throws
 }
 
 fileprivate final class Echo_EchoExpandCallBase: ClientCallServerStreamingBase<Echo_EchoRequest, Echo_EchoResponse>, Echo_EchoExpandCall {
@@ -53,7 +53,7 @@ internal protocol Echo_EchoCollectCall: ClientCallClientStreaming {
   /// Call this to close the connection and wait for a response. Blocking.
   func closeAndReceive() throws -> Echo_EchoResponse
   /// Call this to close the connection and wait for a response. Nonblocking.
-  func closeAndReceive(completion: @escaping (Echo_EchoResponse?, ClientError?) -> Void) throws
+  func closeAndReceive(completion: @escaping (ResultOrRPCError<Echo_EchoResponse>) -> Void) throws
 }
 
 fileprivate final class Echo_EchoCollectCallBase: ClientCallClientStreamingBase<Echo_EchoRequest, Echo_EchoResponse>, Echo_EchoCollectCall {
@@ -68,9 +68,9 @@ class Echo_EchoCollectCallTestStub: ClientCallClientStreamingTestStub<Echo_EchoR
 
 internal protocol Echo_EchoUpdateCall: ClientCallBidirectionalStreaming {
   /// Call this to wait for a result. Blocking.
-  func receive() throws -> Echo_EchoResponse
+  func receive() throws -> Echo_EchoResponse?
   /// Call this to wait for a result. Nonblocking.
-  func receive(completion: @escaping (Echo_EchoResponse?, ClientError?) -> Void) throws
+  func receive(completion: @escaping (ResultOrRPCError<Echo_EchoResponse?>) -> Void) throws
 
   /// Call this to send each message in the request stream.
   func send(_ message: Echo_EchoRequest, completion: @escaping (Error?) -> Void) throws
@@ -210,8 +210,10 @@ fileprivate final class Echo_EchoExpandSessionBase: ServerSessionServerStreaming
 class Echo_EchoExpandSessionTestStub: ServerSessionServerStreamingTestStub<Echo_EchoResponse>, Echo_EchoExpandSession {}
 
 internal protocol Echo_EchoCollectSession: ServerSessionClientStreaming {
-  /// Receive a message. Blocks until a message is received or the client closes the connection.
-  func receive() throws -> Echo_EchoRequest
+  /// Call this to wait for a result. Blocking.
+  func receive() throws -> Echo_EchoRequest?
+  /// Call this to wait for a result. Nonblocking.
+  func receive(completion: @escaping (ResultOrRPCError<Echo_EchoRequest?>) -> Void) throws
 
   /// Send a response and close the connection.
   func sendAndClose(_ response: Echo_EchoResponse) throws
@@ -222,8 +224,10 @@ fileprivate final class Echo_EchoCollectSessionBase: ServerSessionClientStreamin
 class Echo_EchoCollectSessionTestStub: ServerSessionClientStreamingTestStub<Echo_EchoRequest, Echo_EchoResponse>, Echo_EchoCollectSession {}
 
 internal protocol Echo_EchoUpdateSession: ServerSessionBidirectionalStreaming {
-  /// Receive a message. Blocks until a message is received or the client closes the connection.
-  func receive() throws -> Echo_EchoRequest
+  /// Call this to wait for a result. Blocking.
+  func receive() throws -> Echo_EchoRequest?
+  /// Call this to wait for a result. Nonblocking.
+  func receive(completion: @escaping (ResultOrRPCError<Echo_EchoRequest?>) -> Void) throws
 
   /// Send a message. Nonblocking.
   func send(_ response: Echo_EchoResponse, completion: ((Error?) -> Void)?) throws

Sources/Examples/Echo/main.swift
@@ -109,16 +109,13 @@ Group {
       callResult = result
       sem.signal()
     }
-    var running = true
-    while running {
-      do {
-        let responseMessage = try expandCall.receive()
-        print("expand received: \(responseMessage.text)")
-      } catch ClientError.endOfStream {
-        running = false
-      }
+    while true {
+      guard let responseMessage = try expandCall.receive()
+        else { break }  // End of stream
+      print("expand received: \(responseMessage.text)")
     }
     _ = sem.wait()
+
     if let statusCode = callResult?.statusCode {
       print("expand completed with code \(statusCode)")
     }
@@ -150,6 +147,7 @@ Group {
     let responseMessage = try collectCall.closeAndReceive()
     print("collect received: \(responseMessage.text)")
     _ = sem.wait()
+
     if let statusCode = callResult?.statusCode {
       print("collect completed with code \(statusCode)")
     }
@@ -182,17 +180,10 @@ Group {
     try updateCall.closeSend()
 
     while true {
-      do {
-        let responseMessage = try updateCall.receive()
-        print("update received: \(responseMessage.text)")
-      } catch ClientError.endOfStream {
-        break
-      } catch (let error) {
-        print("update receive error: \(error)")
-        break
-      }
+      guard let responseMessage = try updateCall.receive()
+        else { break }  // End of stream
+      print("update received: \(responseMessage.text)")
     }
-
     _ = sem.wait()
 
     if let statusCode = callResult?.statusCode {

Sources/SwiftGRPC/Core/Call.swift
@@ -30,113 +30,6 @@ public enum CallWarning: Error {
   case blocked
 }
 
-public enum CallError: Error {
-  case ok
-  case unknown
-  case notOnServer
-  case notOnClient
-  case alreadyAccepted
-  case alreadyInvoked
-  case notInvoked
-  case alreadyFinished
-  case tooManyOperations
-  case invalidFlags
-  case invalidMetadata
-  case invalidMessage
-  case notServerCompletionQueue
-  case batchTooBig
-  case payloadTypeMismatch
-
-  static func callError(grpcCallError error: grpc_call_error) -> CallError {
-    switch error {
-    case GRPC_CALL_OK:
-      return .ok
-    case GRPC_CALL_ERROR:
-      return .unknown
-    case GRPC_CALL_ERROR_NOT_ON_SERVER:
-      return .notOnServer
-    case GRPC_CALL_ERROR_NOT_ON_CLIENT:
-      return .notOnClient
-    case GRPC_CALL_ERROR_ALREADY_ACCEPTED:
-      return .alreadyAccepted
-    case GRPC_CALL_ERROR_ALREADY_INVOKED:
-      return .alreadyInvoked
-    case GRPC_CALL_ERROR_NOT_INVOKED:
-      return .notInvoked
-    case GRPC_CALL_ERROR_ALREADY_FINISHED:
-      return .alreadyFinished
-    case GRPC_CALL_ERROR_TOO_MANY_OPERATIONS:
-      return .tooManyOperations
-    case GRPC_CALL_ERROR_INVALID_FLAGS:
-      return .invalidFlags
-    case GRPC_CALL_ERROR_INVALID_METADATA:
-      return .invalidMetadata
-    case GRPC_CALL_ERROR_INVALID_MESSAGE:
-      return .invalidMessage
-    case GRPC_CALL_ERROR_NOT_SERVER_COMPLETION_QUEUE:
-      return .notServerCompletionQueue
-    case GRPC_CALL_ERROR_BATCH_TOO_BIG:
-      return .batchTooBig
-    case GRPC_CALL_ERROR_PAYLOAD_TYPE_MISMATCH:
-      return .payloadTypeMismatch
-    default:
-      return .unknown
-    }
-  }
-}
-
-public struct CallResult: CustomStringConvertible {
-  public let statusCode: StatusCode
-  public let statusMessage: String?
-  public let resultData: Data?
-  public let initialMetadata: Metadata?
-  public let trailingMetadata: Metadata?
-
-  fileprivate init(_ op: OperationGroup) {
-    if op.success {
-      if let statusCodeRawValue = op.receivedStatusCode() {
-        if let statusCode = StatusCode(rawValue: statusCodeRawValue) {
-          self.statusCode = statusCode
-        } else {
-          statusCode = .unknown
-        }
-      } else {
-        statusCode = .ok
-      }
-      statusMessage = op.receivedStatusMessage()
-      resultData = op.receivedMessage()?.data()
-      initialMetadata = op.receivedInitialMetadata()
-      trailingMetadata = op.receivedTrailingMetadata()
-    } else {
-      statusCode = .unknown
-      statusMessage = nil
-      resultData = nil
-      initialMetadata = nil
-      trailingMetadata = nil
-    }
-  }
-
-  public var description: String {
-    var result = "status \(statusCode)"
-    if let statusMessage = self.statusMessage {
-      result += ": " + statusMessage
-    }
-    if let resultData = self.resultData {
-      result += "\n"
-      result += resultData.description
-    }
-    if let initialMetadata = self.initialMetadata {
-      result += "\n"
-      result += initialMetadata.description
-    }
-    if let trailingMetadata = self.trailingMetadata {
-      result += "\n"
-      result += trailingMetadata.description
-    }
-    return result
-  }
-}
-
 /// A gRPC API call
 public class Call {
   /// Shared mutex for synchronizing calls to cgrpc_call_perform()
@@ -240,11 +133,18 @@ public class Call {
         .receiveStatusOnClient,
       ]
     case .clientStreaming, .bidiStreaming:
-      operations = [
-        .sendInitialMetadata(metadata.copy()),
-        .receiveInitialMetadata,
-        .receiveStatusOnClient,
-      ]
+      try perform(OperationGroup(call: self,
+                                 operations: [
+                                  .sendInitialMetadata(metadata.copy()),
+                                  .receiveInitialMetadata
+                                  ],
+                                 completion: nil))
+      try perform(OperationGroup(call: self,
+                                 operations: [.receiveStatusOnClient],
+                                 completion: completion != nil
+                                  ? { op in completion?(CallResult(op)) }
+                                  : nil))
+      return
     }
     try perform(OperationGroup(call: self,
                                operations: operations,
@@ -304,27 +204,17 @@ public class Call {
 
   // Receive a message over a streaming connection.
   /// - Throws: `CallError` if fails to call.
-  public func closeAndReceiveMessage(completion: @escaping (Data?) throws -> Void) throws {
+  public func closeAndReceiveMessage(completion: @escaping (CallResult) -> Void) throws {
     try perform(OperationGroup(call: self, operations: [.sendCloseFromClient, .receiveMessage]) { operationGroup in
-      if operationGroup.success {
-        if let messageBuffer = operationGroup.receivedMessage() {
-          try completion(messageBuffer.data())
-        } else {
-          try completion(nil) // an empty response signals the end of a connection
-        }
-      }
+      completion(CallResult(operationGroup))
     })
   }
 
   // Receive a message over a streaming connection.
   /// - Throws: `CallError` if fails to call.
-  public func receiveMessage(completion: @escaping (Data?) throws -> Void) throws {
+  public func receiveMessage(completion: @escaping (CallResult) -> Void) throws {
     try perform(OperationGroup(call: self, operations: [.receiveMessage]) { operationGroup in
-      if operationGroup.success {
-        try completion(operationGroup.receivedMessage()?.data())
-      } else {
-        try completion(nil)
-      }
+      completion(CallResult(operationGroup))
     })
   }
 

commit 93c873e409a1d9650f27b526087ad076f1461327

Fix how the server handles unknown methods (broken by me in 2fdc4dfc, I had forgotten that sending initial metadata is required).

Sources/SwiftGRPC/Core/Handler.swift
@@ -137,7 +137,7 @@ public class Handler {
                                       .receiveCloseOnServer,
                                       .sendStatusFromServer(statusCode, statusMessage, trailingMetadata),
                                       .sendMessage(messageBuffer)
-    ]) { operationGroup in
+    ]) { _ in
       self.shutdown()
     }
     try call.perform(operations)

Sources/SwiftGRPC/Core/OperationGroup.swift
@@ -29,6 +29,7 @@ class OperationGroup {
   let tag: Int64
 
   /// The call associated with the operation group. Retained while the operations are running.
+  // FIXME(danielalm): Is this property needed?
   private let call: Call
 
   /// An array of operation objects that are passed into the initializer.

Sources/SwiftGRPC/Runtime/ServiceServer.swift
@@ -65,13 +65,22 @@ open class ServiceServer {
         + " calling " + unwrappedMethod
         + " from " + unwrappedCaller
         + " with " + handler.requestMetadata.description)
-
+      
       do {
-        if try !strongSelf.handleMethod(unwrappedMethod, handler: handler, queue: queue) {
-          // handle unknown requests
-          try handler.sendStatus(statusCode: .unimplemented,
-                                 statusMessage: "unknown method " + unwrappedMethod,
-                                 trailingMetadata: Metadata())
+        if !(try strongSelf.handleMethod(unwrappedMethod, handler: handler, queue: queue)) {
+          do {
+            try handler.call.perform(OperationGroup(
+              call: handler.call,
+              operations: [
+                .sendInitialMetadata(Metadata()),
+                .receiveCloseOnServer,
+                .sendStatusFromServer(.unimplemented, "unknown method " + unwrappedMethod, Metadata())
+            ]) { _ in
+              handler.shutdown()
+            })
+          } catch {
+            print("ServiceServer.start error sending status for unknown method: \(error)")
+          }
         }
       } catch {
         print("Server error: \(error)")